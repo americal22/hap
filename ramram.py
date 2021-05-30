b3623f19e7b02be73fe3eb5e3e465558bb661193
rmnath <83457221+rmnath@users.noreply.github.com>
2021-05-30 07:15:02 +0530

    Update ramram.py

--- ramram.py
+++ ramram.py
@@ -1,12 +1,8 @@
 #!/opt/conda/bin/python3.7
 # -*- coding: utf-8 -*-
-import os
 import re
-import os
 import sys
 from nuxhash import nuxhashd
-import os
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    import os
     sys.exit(nuxhashd())
