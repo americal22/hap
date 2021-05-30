3fb18f8626a8e19de8ac4cb5a7be6f36acadc5c3
rmnath <83457221+rmnath@users.noreply.github.com>
2021-05-30 07:25:58 +0530

    Update ramram.py

--- ramram.py
+++ ramram.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
-from nuxhash import nuxhashd
+from nuxhash import ram
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
     sys.exit(ram())
