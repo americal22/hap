312cd1c37fee422ebc65847d8b292b4ce95fb768
rmnath <83457221+rmnath@users.noreply.github.com>
2021-05-30 07:35:40 +0530

    Update bg.py

--- bg.py
+++ bg.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
-from nuxhash import ram
+from nuxhash import nuxhashd
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(ram())
+    sys.exit(nuxhashd())
